3d3b4d7f898d6b4a4ef6dededc87761530fa7c89
AnshullKumar <anshustudy21@gmail.com>
2026-02-13 16:33:00 +0530

    Implementation of Bubble Sort

--- Data Structures/Sorting/bubbleSort.java
+++ Data Structures/Sorting/bubbleSort.java
@@ -3,9 +3,25 @@
 import java.util.Scanner;
 public class bubbleSort{
 	public static void main(String[] args){
-		Scanner sc = new Scanner()
-		System.out.print("Enter 5 elements to be inserted into the array: ");
+		Scanner sc = new Scanner();
 		
+		System.out.print("Enter 5 elements to be inserted into the array: \n");
+		int[] array = new int[5];
+		
+		for(int i = 0; i < 5; i++) arr[i] = sc.nextInt();
+
+		System.out.print("\nSuccessfully inserted elements in the array...\n");
+
+		System.out.print("Sorting the array...\n");
+
+		
+		for(int i = 0; i < 5; i++){
+			for(int j = i + 1; j < 5; j++){
+				if(arr[i] > arr[j]){
+					
+				}
+			}
+		}
 
 		
 	}
